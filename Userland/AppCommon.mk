209bb6a548b77dd1400282cbc1cc1502f0efe46b
moon-coffee <194471010+moon-coffee@users.noreply.github.com>
2026-05-27 22:44:19 +0900

    Update ShareLib automated source file include

--- Userland/AppCommon.mk
+++ Userland/AppCommon.mk
@@ -1,5 +1,8 @@
 COMMON_LIBS_DIR := ../../../../Build/Userland
 
+SHARELIB_SRCS := $(shell find ../../../../ShareLib -name "*.c" 2>/dev/null)
+COMMON_SHARELIB_OBJS := $(patsubst ../../../../ShareLib/%.c,../../../../Build/ShareLib/%.o,$(SHARELIB_SRCS))
+
 COMMON_OBJS := $(COMMON_LIBS_DIR)/Syscalls.o \
                $(COMMON_LIBS_DIR)/libc/src/string.o \
                $(COMMON_LIBS_DIR)/libc/src/stdlib.o \
@@ -10,4 +13,5 @@ COMMON_OBJS := $(COMMON_LIBS_DIR)/Syscalls.o \
                $(COMMON_LIBS_DIR)/API/XMLParser.o \
                $(COMMON_LIBS_DIR)/libc/src/math.o \
                $(COMMON_LIBS_DIR)/libc/src/stdio.o \
-               $(COMMON_LIBS_DIR)/NetworkStack/DNS/DNS.o
+               $(COMMON_LIBS_DIR)/NetworkStack/DNS/DNS.o \
+               $(COMMON_SHARELIB_OBJS)
